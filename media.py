0eb1635305bf9a3e47962570972ca35df77a7216
DJG89 <dgallowjr@hotmail.com>
2018-01-25 18:30:07 +0800

    first commit

---
+++ media.py
@@ -0,0 +1,11 @@
+import webbrowser
+class Movie():
+# Movie creates the structure for the multiple arrays in entertainment_center.py
+    def __init__ (self, movie_title, movie_storyline, poster_image, trailer_youtube):
+            self.title = movie_title
+            self.storyline = movie_storyline
+            self.poster_image_url = poster_image
+            self.trailer_youtube_url = trailer_youtube
+                  
+    def show_trailer(self):
+        webbrowser.open(self.trailer_youtube_url)
